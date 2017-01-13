05408aed4e6e7610540ed2719f56b42244e87f3b
halla <halladrianedward@gmail.com>
2017-01-13 18:07:32 +0200

    No Commit Message

--- serverless-rest-api/todos/create.js
+++ serverless-rest-api/todos/create.js
@@ -1,40 +1,39 @@
 "use strict";
 
-const uuid = require("uuid");
-const AWS = require("aws-sdk");
+const uuid = ('uuid');
+const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.create = (event, context, callback) => {
+    
     const timestamp = new Date().getTime();
     const data = JSON.parse(event.body);
 
-    if (typeof data.text !== "string") {
-        console.error("Validation failed.");
-        callback(new Error("Couldn\'t create the todo item."));
+    if (typeof data.text !== 'string') {
+        console.error('Validation failed.');
+        callback(new Error('Could not create the todo item.'));
+        return;
     }
-
     const params = {
-        TableName: "todos",
+        TableName: 'todos',
         Item: {
-            id: uuid.v1(),
+            id:  uuid.v1(),
             text: data.text,
             checked: false,
             createdAt: timestamp,
             updatedAt: timestamp
         }
     }
-
-    dynamoDb.put(params, (error, result)); {
-        if (error) {
-            console.error(error);
-            callback(new Error("Couldn\'t create the todo item"));
+    dynamoDb.put.(params, (error, result)) => {
+        if(error) {
+            consol.error(error);
+            callback(new Error("Could not create the todo item."));
             return;
         }
-
         const response = {
             statusCode: 200,
             body: JSON.stringify(results.Item)
         }
-        callback(null, response);
+        callback(null,response);
     }
 }
